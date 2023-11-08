57f361747f365613ee93be531551b3aeac1dc33c
alexander <alexandernyaga1999@gmail.com>
2023-11-08 19:52:11 +0300

    add Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
 import NavBar from './NavBar';
-
+import Home from './Home';
+import Index from './index';
 function App() {
   return (
     <div className='App'>
       <NavBar />
       <div className='content'>
+      <Home />
+
 
       </div>
     </div>
